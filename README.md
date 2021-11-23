ca81c5b4e90d257d2382ee72859554c0919f275c
Matthew Jin <matjin@microsoft.com>
2021-11-23 09:31:24 -0800

    initial setup

--- README.md
+++ README.md
@@ -1,70 +1,35 @@
 # infersharp-ext README
 
-This is the README for your extension "infersharp-ext". After writing up a brief description, we recommend including the following sections.
+This is an extension which integrates [InferSharp](https://github.com/microsoft/infersharp) into VSCode. Currently, this extension has only been tested for Windows.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+This extension adds the *InferSharp Analysis* command to the Command Palette. 
 
-For example if there is an image subfolder under your extension project workspace:
+If InferSharp has not yet been set up on the machine yet, this command will perform installation by installing WSL Ubuntu if it is not already present and then downloading and unpacking InferSharp binaries to the WSL Linux file system.
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+After setup, subsequent invocations of the command prompt the user to provide the root of a directory tree which contains the .dll and .pdb files produced by the project's build system. Note that these files need only need be somewhere in the tree, as InferSharp will traverse the tree to find the binaries. It conducts the analysis through WSL and copies the resulting infer-out directory to the provided path. The bug report is located at infer-out/report.txt.
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+Please see the requirements for [WSL](https://docs.microsoft.com/en-us/windows/wsl/install). In particular, please check that you are running Windows 10 version 2004 or higher (Build 19041 or higher), or Windows 11. Otherwise, automated setup may fail and you will have to set up WSL manually.
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+Additionally, please ensure that Ubuntu is your default distribution. 
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Automatic setup may fail, particularly if your version of Windows is too old. In this case, please try to run [manual](https://docs.microsoft.com/en-us/windows/wsl/install-manual) installation of WSL. Please set Ubuntu as the default distribution via the -s flag.
+
+After setup, please execute the following commands from Powershell:
+
+```
+wsl -u root --cd '//wsl/Ubuntu/home/root/root' wget https://github.com/microsoft/infersharp/releases/download/v1.2/infersharp-linux64-v1.2.tar.gz -O infersharp.tar.gz
+wsl -u root --cd '//wsl/Ubuntu/home/root/root' tar -xvzf infersharp.tar.gz
+wsl -u root --cd '//wsl/Ubuntu/home/root/root' rm infersharp.tar.gz
+```
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Initial release.
--- src/extension.ts
+++ src/extension.ts
@@ -6,18 +6,15 @@ import * as cp from 'child_process';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+	let infersharpConsole = vscode.window.createOutputChannel("InferSharp");
+	infersharpConsole.show();
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, InferSharp is now active!');
-	let infersharpConsole = vscode.window.createOutputChannel("InferSharp");
-
-	cp.exec('./setup.sh');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('infersharp-ext.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('infersharp-ext.analyze', () => {
 		const options: vscode.OpenDialogOptions = {
 			canSelectMany: false,
 			openLabel: 'Open',
@@ -26,20 +23,50 @@ export function activate(context: vscode.ExtensionContext) {
 			defaultUri: vscode.workspace.workspaceFolders === undefined ? undefined : vscode.workspace.workspaceFolders[0].uri
 	   };
 		// The code you place here will be executed every time your command is executed
-		vscode.window.showOpenDialog(options).then(fileUri => {
-			if (fileUri && fileUri[0]) {
-				cp.exec('./run.sh ' + fileUri, (err, stdout, stderr) => {
-					infersharpConsole.appendLine('stdout: ' + stdout);
-					infersharpConsole.appendLine('stderr: ' + stderr);
-					if (err) {
-						infersharpConsole.appendLine('error: ' + err);
-					}
-				});
-			}
-			else {
-				infersharpConsole.appendLine("No valid folder given.");
-			}
-		});
+		let res = cp.spawnSync("wsl -u root --cd '//wsl/Ubuntu/home/root/root' ls infersharp", { shell: 'powershell.exe' });
+		if (res.status !== 0) {
+			infersharpConsole.appendLine("Starting install; please wait while Infer# binaries are downloaded and extracted.");
+			let analysisCommands = [
+				"wsl --install -d ubuntu",
+				"do {wsl -d ubuntu -u root --cd '//wsl/Ubuntu/home/root/root' wget https://github.com/microsoft/infersharp/releases/download/v1.2/infersharp-linux64-v1.2.tar.gz -O infersharp.tar.gz; $success =$?; Start-Sleep -s 5;} until ($success);",
+				"wsl -d ubuntu -u root --cd '//wsl/Ubuntu/home/root/root' tar -xvzf infersharp.tar.gz",
+				"wsl -d ubuntu -u root --cd '//wsl/Ubuntu/home/root/root' rm infersharp.tar.gz",
+				"wsl -s ubuntu",
+				"echo 'Setup complete. You may now run Infer#!'"
+			];
+			let setup = cp.spawn(analysisCommands.join(' ; '), { shell: 'powershell.exe' });
+			setup.stdout.on('data', data => infersharpConsole.append(data.toString()));
+			setup.stderr.on('data', data => infersharpConsole.append(data.toString()));
+		}
+		else {
+			infersharpConsole.clear();
+			vscode.window.showOpenDialog(options).then(fileUri => {
+				if (fileUri && fileUri[0]) {
+					let inputPath = "//mnt" + fileUri[0].path.replace('C:', 'c');
+					infersharpConsole.appendLine('InferSharp is analyzing: ' + fileUri[0].path.replace('C:', 'c'));
+					let analysisCommands = [
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' rm -rf infer-out/",
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' cp infersharp/Cilsil/System.Private.CoreLib.dll " + inputPath + "/System.Private.CoreLib.dll",
+						"wsl echo 'Beginning translation.'",
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' infersharp/Cilsil/Cilsil translate " + inputPath + " --outcfg " + inputPath + "/cfg.json " + " --outtenv " + inputPath + "/tenv.json",
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' mv " + inputPath + "/cfg.json" + " //cfg.json",
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' mv " + inputPath + "/tenv.json" + " //tenv.json",
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' rm -rf " + inputPath + "/System.Private.CoreLib.dll",
+						"wsl echo 'Translation complete. Beginning analysis.'",
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' infersharp/infer/lib/infer/infer/bin/infer capture",
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' mkdir -p infer-out/captured",
+						"wsl -u root --cd '//wsl/Ubuntu/home/root/root' infersharp/infer/lib/infer/infer/bin/infer analyzejson --debug-level 1 --pulse --no-biabduction --cfg-json cfg.json --tenv-json tenv.json",
+						"wsl -u root cp -r //infer-out/ //mnt" + fileUri[0].path.replace('C:', 'c'),
+					];
+					let child = cp.spawn(analysisCommands.join(' ; '), [], { shell: 'powershell.exe' });
+					child.stdout.on('data', data => infersharpConsole.append(data.toString()));
+					child.stderr.on('data', data => infersharpConsole.append(data.toString()));
+				}
+				else {
+					infersharpConsole.appendLine("No valid folder given.");
+				}
+			});
+		}
 	});
 
 	context.subscriptions.push(disposable);
